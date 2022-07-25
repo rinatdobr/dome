ed529532c6b0cca5e2ad1d83e0c4f3bf8a8ec67f
Rinat Dobrokhotov <rinatdobr@outlook.com>
2022-07-25 21:12:12 +0300

    Init

---
+++ service/dome.h
@@ -0,0 +1,22 @@
+#ifndef DOME_H
+#define DOME_H
+
+#include <QtCore/QObject>
+#include <QVariantMap>
+
+#include "sensors.h"
+
+class Dome: public QObject
+{
+    Q_OBJECT
+public:
+    virtual ~Dome();
+
+public slots:
+    Q_SCRIPTABLE QVariantMap systemSensors();
+
+private:
+    Sensors m_sensors;
+};
+
+#endif
---
+++ service/sensors.cpp
@@ -0,0 +1,6 @@
+#include "sensors.h"
+
+double Sensors::cpuTemparture() const
+{
+    return 10.5;
+}
