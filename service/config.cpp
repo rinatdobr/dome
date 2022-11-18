509a1d0021c113d3a44bbbc90a738c789bff7b41
Rinat Dobrokhotov <rinatdobr@outlook.com>
2022-11-18 08:14:44 +0300

    Remove legacy code

--- service/config.cpp
+++
@@ -1,9 +0,0 @@
-#include "config.h"
-
-#include <spdlog/spdlog.h>
-
-Config::Config()
-    : dome::config::IpCamera(std::getenv("DOME_IP_CAMERA_CONFIG"))
-{
-    spdlog::trace("{}:{} {}", __FILE__, __LINE__, __PRETTY_FUNCTION__);
-}
